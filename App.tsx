9cb7414b9585c6bc0199e6c3542f35958fe69d2c
jcm2512 <joe.magalotti@gmail.com>
2022-12-15 13:41:01 +0900

    signup

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './config/firebase';
 import RootNavigation from './navigation';
+import { ThemeProvider, Button } from 'react-native-elements';
 
 export default function App() {
   return (
+    // <ThemeProvider>
     <RootNavigation />
+    // </ThemeProvider>
   );
 }
