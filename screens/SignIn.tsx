fa014a32ab62f244268bb4c8948987438c9e1ae5
jcm2512 <joe.magalotti@gmail.com>
2022-12-15 13:52:22 +0900

    authentication done

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -1,12 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input, Button } from 'react-native-elements';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
-export default function HomeScreen() {
+const auth = getAuth();
+
+const SignInScreen = () => {
+  const [value, setValue] = React.useState({
+    email: '',
+    password: '',
+    error: ''
+  })
+
+  async function signIn() {
+    if (value.email === '' || value.password === '') {
+      setValue({
+        ...value,
+        error: 'Email and password are mandatory.'
+      })
+      return;
+    }
+
+  
+    signInWithEmailAndPassword(auth, value.email, value.password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+  
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Home screen!</Text>
-      <StatusBar style="auto" />
+      <Text>Signin screen!</Text>
+
+      {!!value.error && <View style={styles.error}><Text>{value.error}</Text></View>}
+
+      <View style={styles.controls}>
+        <Input
+          placeholder='Email'
+          containerStyle={styles.control}
+          value={value.email}
+          onChangeText={(text) => setValue({ ...value, email: text })}
+          leftIcon={<Icon
+            name='envelope'
+            size={16} />} autoCompleteType={undefined}        />
+
+        <Input
+          placeholder='Password'
+          containerStyle={styles.control}
+          value={value.password}
+          onChangeText={(text) => setValue({ ...value, password: text })}
+          secureTextEntry={true}
+          leftIcon={<Icon
+            name='key'
+            size={16} />} autoCompleteType={undefined}        />
+
+        <Button title="Sign in" buttonStyle={styles.control} onPress={signIn} />
+      </View>
     </View>
   );
 }
@@ -14,8 +72,26 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  controls: {
+    flex: 1,
+  },
+
+  control: {
+    marginTop: 10
+  },
+
+  error: {
+    marginTop: 10,
+    padding: 10,
+    color: '#fff',
+    backgroundColor: '#D54826FF',
+  }
 });
+
+export default SignInScreen;
