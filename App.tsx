a83dc23af4b88d2c827736b15327e598e2e31db1
jcm2512 <joe.magalotti@gmail.com>
2022-12-23 01:52:06 +0900

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import './config/firebase';
-import RootNavigation from './navigation';
-import { ThemeProvider, Button } from 'react-native-elements';
+import React from "react";
+import "./config/firebase";
+import RootNavigation from "./navigation";
+import { ThemeProvider, Button } from "react-native-elements";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Lexend-Bold": require("./assets/fonts/Lexend-Bold.ttf"),
+  });
   return (
     // <ThemeProvider>
     <RootNavigation />
